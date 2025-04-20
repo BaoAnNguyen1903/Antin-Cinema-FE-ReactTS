21b3b1f96ff70b904aaf1bb5f9090ed7d80d41f5
An Nguyen <anndb1903@gmail.com>
2025-04-20 11:48:12 +0700

    update

--- src/components/layout/app.footer.tsx
+++ src/components/layout/app.footer.tsx
@@ -1,9 +1,5 @@
 const AppFooter = () => {
-    return (
-        <div className="text-center my-3">
-            Made by Nguyen Dinh Bao An
-        </div>
-    )
-}
+  return <div className="text-center my-3">Made by Nguyen Dinh Bao An</div>;
+};
 
-export default AppFooter
+export default AppFooter;
